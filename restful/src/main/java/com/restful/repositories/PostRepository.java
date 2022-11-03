8ae5f9e2cc73528392c5ed448f9ed43fe10906eb
Daniel Augusto <danielpn23@outlook.com>
2022-11-03 10:50:18 -0300

    Post Repository

---
+++ restful/src/main/java/com/restful/repositories/PostRepository.java
@@ -0,0 +1,12 @@
+package com.restful.repositories;
+
+import com.restful.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+}
