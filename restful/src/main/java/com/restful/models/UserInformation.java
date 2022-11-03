10bf37b0745df64f319a2844f50f615e956860e4
Daniel Augusto <danielpn23@outlook.com>
2022-11-03 10:49:55 -0300

    Implementing a class to transfer essencial information about a user

---
+++ restful/src/main/java/com/restful/models/UserInformation.java
@@ -0,0 +1,40 @@
+package com.restful.models;
+
+import java.util.List;
+
+public class UserInformation {
+    private String firstName;
+    private String lastName;
+
+    private List<Post> posts;
+
+    public UserInformation(String firstName, String lastName, List<Post> posts) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.posts = posts;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+}
