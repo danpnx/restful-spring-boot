4e4893f8394d0c48e4b0b546540abed2669b6414
Daniel Augusto <danielpn23@outlook.com>
2022-11-03 10:47:05 -0300

    New endpoints for user

--- restful/src/main/java/com/restful/controllers/UserController.java
+++ restful/src/main/java/com/restful/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.restful.controllers;
 
 import com.restful.models.User;
+import com.restful.models.UserInformation;
 import com.restful.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -8,11 +9,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,18 +26,54 @@ public class UserController {
     private UserService userService;
 
     @Operation(
-            summary = "Find all users", description = "Temporary endpoint", tags = {"User"},
+            summary = "Find user info", description = "Get user information", tags = {"User"},
             responses = {
                     @ApiResponse(description = "OK", responseCode = "200",
                     content = @Content(
                             mediaType = "application/json",
                             array = @ArraySchema(schema = @Schema(implementation = User.class))
                     )
-                    )
+                    ),
+                    @ApiResponse(description = "FORBIDDEN", responseCode = "403", content = @Content)
             }
     )
-    @GetMapping("/all")
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok(userService.findAll());
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserInfo() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+
+        return ResponseEntity.ok(userService.getUserInfo(username));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserInformation> getUserByUsername(@PathVariable(value = "username") String username) {
+        return ResponseEntity.ok(userService.getUserByUsername(username));
+    }
+
+    @PutMapping("/config/privacy")
+    public ResponseEntity<?> updateAccountVisibility(@RequestParam boolean isPrivate) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        userService.updateAccountVisibility(isPrivate, username);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/config/update-password")
+    public ResponseEntity<?> updatePassword(@RequestParam String newPassword) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        userService.updatePassword(newPassword, username);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/config/update-first-name")
+    public ResponseEntity<?> updateFirstName(@RequestParam String firstName) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        userService.updateFirstName(firstName, username);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/config/update-last-name")
+    public ResponseEntity<?> updateLastName(@RequestParam String lastName) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        userService.updateLastName(lastName, username);
+        return ResponseEntity.ok().build();
     }
 }
